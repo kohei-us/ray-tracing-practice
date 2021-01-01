6be0220de16101cf964f1b5f0aa2878b5e16fbfc
Kohei Yoshida <kohei.yoshida@gmail.com>
2020-12-31 21:58:11 -0500

    Draw surface normals and simplify hit_sphere().

--- make_ppm.cpp
+++ make_ppm.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-bool hit_sphere(const point3& center, double radius, const ray& r)
+double hit_sphere(const point3& center, double radius, const ray& r)
 {
     // (P - C)*(P - C) = r^2 where P = A + t*b
     //
@@ -22,22 +22,32 @@ bool hit_sphere(const point3& center, double radius, const ray& r)
     //
     // (-b +- sqrt(b^2 - 4ac)) / 2a
 
-    vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
+    // If b = 2h, then
+    // (-h +- sqrt(h^2 - ac)) / a
+    // With this, use half_b = h to simplify the equation.
 
-    return discriminant > 0;
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
+
+    return discriminant < 0 ? -1.0 : (-half_b - sqrt(discriminant)) / a;
 }
 
 color ray_color(const ray& r)
 {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0)
+    {
+        // inside the sphere. Use surface normal as color.
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1)); // surface normal at t
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
 
+    // outside the sphere. Draw background color.
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0); // scale t to [0-1]
+    t = 0.5 * (unit_direction.y() + 1.0); // scale t to [0-1]
     return (1.0 - t) * color(1.0, 0.5, 0.2) + t * color(0.5, 0.7, 1.0);
 }
 
