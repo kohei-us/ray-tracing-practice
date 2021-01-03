e8c08128c9e7d8e8119f7a453a9ddc55e2fa3840
Kohei Yoshida <kohei.yoshida@gmail.com>
2021-01-03 17:05:23 -0500

    Apply fuzz to reflection.

--- include/material.h
+++ include/material.h
@@ -28,11 +28,12 @@ public:
 class metal : public material
 {
 public:
-    metal(const color& a) : albedo(a) {}
+    metal(const color& a, double f) : albedo(a), fuzz(f < 1.0 ? f : 1.0) {}
 
     virtual bool scatter(
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override;
 
 public:
     color albedo;
+    double fuzz;
 };
