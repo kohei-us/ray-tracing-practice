4d5773cebbdc4ea281cfa9362b7b3f9ee7c2cc63
Kohei Yoshida <kohei.yoshida@gmail.com>
2021-01-25 19:13:57 -0500

    Add xz_rect and yz_rect to the mix.

--- aarect.cpp
+++ aarect.cpp
@@ -39,3 +39,76 @@ bool xy_rect::bounding_box(double time0, double time1, aabb &output_box) const
     return true;
 }
 
+xz_rect::xz_rect() {}
+xz_rect::xz_rect(double _x0, double _x1, double _z0, double _z1, double _k, const std::shared_ptr<material>& _mp) :
+    x0(_x0), x1(_x1), z0(_z0), z1(_z1), k(_k), mp(_mp) {}
+
+bool xz_rect::hit(const ray &r, double t_min, double t_max, hit_record &rec) const
+{
+    // Determine t where z == k
+    double t = (k - r.origin().y()) / r.direction().y();
+
+    if (t < t_min || t > t_max)
+        return false;
+
+    double x = r.origin().x() + t * r.direction().x();
+    double z = r.origin().z() + t * r.direction().z();
+
+    if (x < x0 || x > x1 || z < z0 || z > z1)
+        return false;
+
+    rec.u = (x-x0) / (x1-x0);
+    rec.v = (z-z0) / (z1-z0);
+    rec.t = t;
+
+    auto outward_normal = vec3(0, 1, 0);
+
+    rec.set_face_normal(r, outward_normal);
+    rec.mat_ptr = mp;
+    rec.p = r.at(t);
+
+    return true;
+}
+
+bool xz_rect::bounding_box(double time0, double time1, aabb &output_box) const
+{
+    output_box = aabb(point3(x0, k - 0.0001, z0), point3(x1, k + 0.0001, z1));
+    return true;
+}
+
+yz_rect::yz_rect() {}
+yz_rect::yz_rect(double _y0, double _y1, double _z0, double _z1, double _k, const std::shared_ptr<material>& _mp) :
+    y0(_y0), y1(_y1), z0(_z0), z1(_z1), k(_k), mp(_mp) {}
+
+bool yz_rect::hit(const ray &r, double t_min, double t_may, hit_record &rec) const
+{
+    // Determine t where z == k
+    double t = (k - r.origin().x()) / r.direction().x();
+
+    if (t < t_min || t > t_may)
+        return false;
+
+    double y = r.origin().y() + t * r.direction().y();
+    double z = r.origin().z() + t * r.direction().z();
+
+    if (y < y0 || y > y1 || z < z0 || z > z1)
+        return false;
+
+    rec.u = (y-y0) / (y1-y0);
+    rec.v = (z-z0) / (z1-z0);
+    rec.t = t;
+
+    auto outward_normal = vec3(1, 0, 0);
+
+    rec.set_face_normal(r, outward_normal);
+    rec.mat_ptr = mp;
+    rec.p = r.at(t);
+
+    return true;
+}
+
+bool yz_rect::bounding_box(double time0, double time1, aabb &output_box) const
+{
+    output_box = aabb(point3(k - 0.0001, y0, z0), point3(k + 0.0001, y1, z1));
+    return true;
+}
--- include/aarect.h
+++ include/aarect.h
@@ -19,3 +19,37 @@ public:
     std::shared_ptr<material> mp;
     double x0, x1, y0, y1, k;
 };
+
+/**
+ * 2D rectangle in X-Z plane where Y is constant (=k).
+ */
+class xz_rect : public hittable
+{
+public:
+    xz_rect();
+    xz_rect(double _x0, double _x1, double _z0, double _z1, double _k, const std::shared_ptr<material>& _mp);
+
+    virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const override;
+    virtual bool bounding_box(double time0, double time1, aabb &output_box) const override;
+
+public:
+    std::shared_ptr<material> mp;
+    double x0, x1, z0, z1, k;
+};
+
+/**
+ * 2D rectangle in Y-Z plane where X is constant (=k).
+ */
+class yz_rect : public hittable
+{
+public:
+    yz_rect();
+    yz_rect(double _y0, double _y1, double _z0, double _z1, double _k, const std::shared_ptr<material>& _mp);
+
+    virtual bool hit(const ray &r, double t_min, double t_may, hit_record &rec) const override;
+    virtual bool bounding_box(double time0, double time1, aabb &output_box) const override;
+
+public:
+    std::shared_ptr<material> mp;
+    double y0, y1, z0, z1, k;
+};
