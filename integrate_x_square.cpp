d612de889c72fdb941af9c690cd86dbfb5247a38
Kohei Yoshida <kohei.yoshida@gmail.com>
2021-02-03 21:02:37 -0500

    Integration of x^2 for x = 0-2 using PDF.

--- integrate_x_square.cpp
+++ integrate_x_square.cpp
@@ -5,18 +5,23 @@
 
 using namespace std;
 
+inline double pdf(double x)
+{
+    return 0.5 * x;
+}
+
 int main(int argc, char **argv)
 {
     int N = 1000000;
     double sum = 0.0;
     for (int i = 0; i < N; ++i)
     {
-        double x = random_double(0, 2);
-        sum += x * x;
+        double x = sqrt(random_double(0, 4));
+        sum += x * x / pdf(x);
     }
 
     cout << fixed << setprecision(12);
-    cout << "I = " << 2 * sum / N << endl;
+    cout << "I = " << sum / N << endl;
 
     return EXIT_SUCCESS;
 }
